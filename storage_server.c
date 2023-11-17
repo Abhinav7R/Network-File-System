d08660402c3651e796c669f31eca4dde40fbcff8
agrimmmm <agrim.mittal@students.iiit.ac.in>
2023-11-18 02:57:12 +0530

    read code

--- storage_server.c
+++ storage_server.c
@@ -1,4 +1,8 @@
 #include "headers.h"
+#define PORT 4545
+char* read = "read";
+char* write = "write";
+char* retrieve = "retrieve";
 
 typedef struct filepaths* files;
 typedef struct filepaths
@@ -7,75 +11,20 @@ typedef struct filepaths
     files next;
 }filepaths;
 
-
-//storage server implementation for NFS
-int main(int argc, char *argv[])
+typedef struct threadargs* args;
+typedef struct threadargs
 {
-    if(argc != 4)
-        printf("Usage: %s <ip> <nm_port> <client_port>\n", argv[0]);
-    
-    char* ip = (char*)calloc(sizeof(char), 21);
-    strcpy(ip, argv[1]);
-    int nm_port = atoi(argv[2]);
-    int client_port = atoi(argv[3]);
-
-    printf("IP: %s\nNM_PORT: %d\nCLIENT_PORT: %d\nEnter file paths(relative to current directory) that the clients can access: (Enter 'DONE' when finished)\n", ip, nm_port, client_port);
-
-    files head = (files)malloc(sizeof(filepaths));
-    files temp = head;
-    while(1)
-    {
-        char* path = (char*)calloc(sizeof(char), 100);
-        scanf("%s", path);
-        if(strcmp(path, "DONE") == 0)
-            break;
-        temp->name = path;
-        temp->next = (files)malloc(sizeof(filepaths));
-        temp = temp->next;
-    }
-    
-    int server_sock_1, server_sock_2, nm_sockfd, client_sockfd;
-    struct sockaddr_in server_addr_1, server_addr_2, nm_addr, client_addr;
     socklen_t addr_size;
-    char buffer_nm[1024], buffer_client[1024];
+    struct sockaddr_in server_addr;
+    int server_sock;
+    int sockfd;
+    char* buffer;
+}threadargs;
 
-    server_sock_1 = socket(AF_INET, SOCK_DGRAM, 0);
-    if(server_sock_1 < 0)
-    {
-        perror("[-]Socket error");
-        exit(1);
-    }
-    printf("[+]TCP server socket created.\n");
-
-    memset(&server_addr_1, '\0', sizeof(server_addr_1));
-    server_addr_1.sin_family = AF_INET;
-    server_addr_1.sin_port = htons(nm_port);
-    server_addr_1.sin_addr.s_addr = inet_addr(ip);
-
-    int n = bind(server_sock_1, (struct sockaddr*)&server_addr_1, sizeof(server_addr_1));
-    if(n < 0)
-    {
-        perror("[-]Bind error");
-        exit(1);
-    }
-    printf("[+]Bind to port %d\n", nm_port);
-
-    if(listen(server_sock_1, 5) < 0)
-    {
-        perror("[-]Listen error");
-        exit(1);
-    }
-    addr_size = sizeof(nm_addr);
-    nm_sockfd = accept(server_sock_1, (struct sockaddr*)&nm_addr, &addr_size);
-    if(nm_sockfd < 0)
-    {
-        perror("[-]Accept error");
-        exit(1);
-    }
-    printf("[+]Naming server connected.\n");
-
+void* nm_handler(char* buffer_nm, int nm_sockfd, int port, int client_port, char* ip, char* paths_of_all)
+{
     bzero(buffer_nm, 1024);
-    sscanf(buffer_nm, "IP: %s\nNM_PORT: %d\nCLIENT PORT: %d\n", ip, &nm_port, &client_port);
+    sscanf(buffer_nm, "IP: %s\nNM_PORT: %d\nCLIENT PORT: %d\n", ip, &port, &client_port);
     if(send(nm_sockfd, buffer_nm, sizeof(buffer_nm), 0) < 0)
     {
         perror("[-]Send error");
@@ -87,36 +36,39 @@ int main(int argc, char *argv[])
         perror("[-]Receive error");
         exit(1);
     }
-    while(1)
-    {
-        bzero(buffer_nm, 1024);
-        if(head != NULL)
-        {
-            strcpy(buffer_nm, head->name);
-            head = head->next;
-            if(send(nm_sockfd, buffer_nm, sizeof(buffer_nm), 0) < 0)
-            {
-                perror("[-]Send error");
-                exit(1);
-            }
-            bzero(buffer_nm, 1024);
-            if(recv(nm_sockfd, buffer_nm, sizeof(buffer_nm), 0) < 0)
-            {
-                perror("[-]Receive error");
-                exit(1);
-            }
-        }
-        else
-            break;
-    }
-
-    bzero(buffer_nm, 1024);
-    strcpy(buffer_nm, "Ready to receive");
-    if(send(nm_sockfd, buffer_nm, sizeof(buffer_nm), 0) < 0)
+    if(send(nm_sockfd, paths_of_all, sizeof(paths_of_all), 0) < 0)
     {
         perror("[-]Send error");
         exit(1);
     }
+    bzero(buffer_nm, 1024);
+    if(recv(nm_sockfd, buffer_nm, sizeof(buffer_nm), 0) < 0)
+    {
+        perror("[-]Receive error");
+        exit(1);
+    }
+
+    close(nm_sockfd);
+}
+
+void nm_handler_for_ops(void* arg)
+{
+    args args_nm = (args)arg;
+    socklen_t addr_size = args_nm->addr_size;
+    struct sockaddr_in server_addr_1 = args_nm->server_addr;
+    int server_sock_1 = args_nm->server_sock;
+    int nm_sockfd = args_nm->sockfd;
+    char* buffer_nm = args_nm->buffer;
+
+    addr_size = sizeof(server_addr_1);
+    nm_sockfd = accept(server_sock_1, (struct sockaddr*)&server_addr_1, &addr_size);
+    if(nm_sockfd < 0)
+    {
+        perror("[-]Accept error");
+        exit(1);
+    }
+    printf("[+]Naming server connected.\n");
+    bzero(buffer_nm, 1024);
 
     while(1)
     {
@@ -126,190 +78,276 @@ int main(int argc, char *argv[])
             perror("[-]Receive error");
             exit(1);
         }
-        if(strcmp(buffer_nm, "READ") == 0)
+        if(strncmp(buffer_nm, read, strlen(read)) == 0)
         {
+            char* token = strtok(buffer_nm, " ");
+            token = strtok(NULL, " ");
+            char* file = token;
+            FILE* fd = fopen(file, "rb");
+            if(fd == NULL)
+            {
+                perror("[-]File open error");
+                exit(1);
+            }
+            fseek(fd, 0, SEEK_END);
+            long size = ftell(fd);
+            fseek(fd, 0, SEEK_SET);
+            fclose(fd);
+            long num_packets = size/1024;
+            if(size%1024 != 0)
+                num_packets++;
             bzero(buffer_nm, 1024);
-            strcpy(buffer_nm, "Send name of file");
+            sprintf(buffer_nm, "%ld", num_packets);
             if(send(nm_sockfd, buffer_nm, sizeof(buffer_nm), 0) < 0)
             {
                 perror("[-]Send error");
                 exit(1);
             }
-
-            bzero(buffer_nm, 1024);
-            if(recv(nm_sockfd, buffer_nm, sizeof(buffer_nm), 0) < 0)
-            {
-                perror("[-]Receive error");
-                exit(1);
-            }
-            char* file = (char*)calloc(sizeof(char), 100);
-            strcpy(file, buffer_nm);
-            
-            server_sock_2 = socket(AF_INET, SOCK_DGRAM, 0);
-            if(server_sock_2 < 0)
-            {
-                perror("[-]Socket error");
-                exit(1);
-            }
-
-            memset(&server_addr_2, '\0', sizeof(server_addr_2));
-            server_addr_2.sin_family = AF_INET;
-            server_addr_2.sin_port = htons(client_port);
-            server_addr_2.sin_addr.s_addr = inet_addr(ip);
-
-            if(bind(server_sock_2, (struct sockaddr*)&server_addr_2, sizeof(server_addr_2)) < 0)
-            {
-                perror("[-]Bind error");
-                exit(1);
-            }
-            int fd = open(file, O_RDONLY);
-            if(fd < 0)
+            if(open(file, O_RDONLY) < 0)
             {
                 perror("[-]File open error");
                 exit(1);
             }
-            
-            while(read(fd, buffer_client, 1024) > 0)
+            while(num_packets--)
             {
-                bzero(buffer_client, 1024);
-                if(send(server_sock_2, buffer_client, sizeof(buffer_client), 0) < 0)
-                {
-                    perror("[-]Send error");
-                    exit(1);
-                }
-                bzero(buffer_client, 1024);
-                if(recv(server_sock_2, buffer_client, sizeof(buffer_client), 0) < 0)
-                {
-                    perror("[-]Receive error");
-                    exit(1);
-                }
-            }
-            close(fd);
-            close(server_sock_2);    
-        }
-        else if(strcmp(buffer_nm, "WRITE") == 0)
-        {
-            bzero(buffer_nm, 1024);
-            strcpy(buffer_nm, "Send name of file");
-            if(send(nm_sockfd, buffer_nm, sizeof(buffer_nm), 0) < 0)
-            {
-                perror("[-]Send error");
-                exit(1);
-            }
-
-            bzero(buffer_nm, 1024);
-            if(recv(nm_sockfd, buffer_nm, sizeof(buffer_nm), 0) < 0)
-            {
-                perror("[-]Receive error");
-                exit(1);
-            }
-            char* file = (char*)calloc(sizeof(char), 100);
-            strcpy(file, buffer_nm);
-
-            server_sock_2 = socket(AF_INET, SOCK_DGRAM, 0);
-            if(server_sock_2 < 0)
-            {
-                perror("[-]Socket error");
-                exit(1);
-            }
-
-            memset(&server_addr_2, '\0', sizeof(server_addr_2));
-            server_addr_2.sin_family = AF_INET;
-            server_addr_2.sin_port = htons(client_port);
-            server_addr_2.sin_addr.s_addr = inet_addr(ip);
-
-            if(bind(server_sock_2, (struct sockaddr*)&server_addr_2, sizeof(server_addr_2)) < 0)
-            {
-                perror("[-]Bind error");
-                exit(1);
-            }
-            int fd = open(file, O_APPEND | O_WRONLY | O_CREAT, 0644);
-            if(fd < 0)
-            {
-                perror("[-]File open error");
-                exit(1);
-            }
-
-            while(1)
-            {
-                bzero(buffer_client, 1024);
-                if(recv(server_sock_2, buffer_client, sizeof(buffer_client), 0) < 0)
-                {
-                    perror("[-]Receive error");
-                    exit(1);
-                }
-                if(strcmp(buffer_client, "EOF") == 0)
-                    break;
-                lseek(fd, 0, SEEK_END);
-                write(fd, buffer_client, strlen(buffer_client));
-                bzero(buffer_client, 1024);
-                strcpy(buffer_client, "Received");
-                if(send(server_sock_2, buffer_client, sizeof(buffer_client), 0) < 0)
+                bzero(buffer_nm, 1024);
+                if(send(nm_sockfd, buffer_nm, sizeof(buffer_nm), 0) < 0)
                 {
                     perror("[-]Send error");
                     exit(1);
                 }
             }
-            close(fd);
-            close(server_sock_2);
         }
-        else if(strcmp(buffer_nm, "RETRIEVE") == 0)
+        else if(strncmp(buffer_nm, write, sizeof(write)) == 0)
         {
-            bzero(buffer_nm, 1024);
-            strcpy(buffer_nm, "Send name of file");
-            if(send(nm_sockfd, buffer_nm, sizeof(buffer_nm), 0) < 0)
-            {
-                perror("[-]Send error");
-                exit(1);
-            }
-            bzero(buffer_nm, 1024);
-            if(recv(nm_sockfd, buffer_nm, sizeof(buffer_nm), 0) < 0)
-            {
-                perror("[-]Receive error");
-                exit(1);
-            }
-            char* file = (char*)calloc(sizeof(char), 100);
-            strcpy(file, buffer_nm);
-
-            server_sock_2 = socket(AF_INET, SOCK_DGRAM, 0);
-            if(server_sock_2 < 0)
-            {
-                perror("[-]Socket error");
-                exit(1);
-            }
-
-            memset(&server_addr_2, '\0', sizeof(server_addr_2));
-            server_addr_2.sin_family = AF_INET;
-            server_addr_2.sin_port = htons(client_port);
-            server_addr_2.sin_addr.s_addr = inet_addr(ip);
-
-            if(bind(server_sock_2, (struct sockaddr*)&server_addr_2, sizeof(server_addr_2)) < 0)
-            {
-                perror("[-]Bind error");
-                exit(1);
-            }
-        }
-        else if(strcmp(buffer_nm, "CREATE") == 0)
-        {
-
-        }
-        else if(strcmp(buffer_nm, "DELETE") == 0)
-        {
-
-        }
-        else if(strcmp(buffer_nm, "COPY") == 0)
-        {
-
-        }
-        else if(strcmp(buffer_nm, "EXIT") == 0)
-        {
-            printf("[+]Exiting.\n");
-            break;
-        }
-        else
-        {
-            printf("[-]Invalid command received.\n");
-            break;
+            char* token = strtok(buffer_nm, " ");
+            token = strtok(NULL, " ");
+            char* file = token;
+            
         }
     }
+    close(nm_sockfd);
+}
+
+void client_handler(void* arg)
+{
+    
+}
+
+
+//storage server implementation for NFS
+int main(int argc, char *argv[])
+{
+    if(argc != 3)
+    {
+        printf("Usage: %s <nm_port> <client_port>\n", argv[0]);
+        exit(1);
+    }
+    
+    char* ip = "127.0.0.1";
+    int nm_port = atoi(argv[2]);
+    int client_port = atoi(argv[3]);
+
+    printf("IP: %s\nNM_PORT: %d\nCLIENT_PORT: %d\nEnter file paths(relative to current directory) that the clients can access: (Enter 'DONE' when finished)\n", ip, nm_port, client_port);
+
+    files head = (files)malloc(sizeof(filepaths));
+    head->next = NULL;
+    files temp = head;
+    while(1)
+    {
+        char* path = (char*)calloc(sizeof(char), 100);
+        scanf("%s", path);
+        if(strcmp(path, "DONE") == 0)
+            break;
+        temp->name = path;
+        temp->next = (files)malloc(sizeof(filepaths));
+        temp = temp->next;
+        temp->next = NULL;
+    }
+
+    char* paths_of_all = (char*)malloc(sizeof(char)*60000);
+    strcpy(paths_of_all, head->name);
+    head = head->next;
+    while(head != NULL)
+    {
+        strcat(paths_of_all, "|");
+        strcat(paths_of_all, head->name);
+        head = head->next;
+    }
+    
+    int socky, server_sock_1, server_sock_2, nm_sockfd, client_sockfd;
+    struct sockaddr_in server_addr_1, server_addr_2, nm_addr, cliett_addr;
+    socklen_t addr_size;
+    char buffer_nm[1024], buffer_client[1024];
+
+    socky = socket(PF_INET, SOCK_STREAM, 0);
+    if(server_sock_1 < 0)
+    {
+        perror("[-]Socket error");
+        exit(1);
+    }
+    // printf("[+]TCP server socket created.\n");
+
+    memset(&server_addr_1, '\0', sizeof(server_addr_1));
+    server_addr_1.sin_family = AF_INET;
+    server_addr_1.sin_port = htons(PORT);
+    server_addr_1.sin_addr.s_addr = inet_addr(ip);
+
+    if(connect(server_sock_1, (struct sockaddr*)&server_addr_1, sizeof(server_addr_1)) == -1)
+    {
+        perror("connect() error");
+        exit(1);
+    }
+    nm_handler(buffer_nm, socky, PORT, client_port, ip, paths_of_all);
+
+    //Creating threads for concurrent working of various clients
+    // pthread_t tid[10];
+    // int i = 0;
+    // while(1)
+    // {
+    //     addr_size = sizeof(server_addr_2);
+    //     client_sockfd = accept(server_sock_2, (struct sockaddr*)&server_addr_2, &addr_size);
+    //     if(client_sockfd < 0)
+    //     {
+    //         perror("[-]Accept error");
+    //         exit(1);
+    //     }
+    //     printf("[+]Client connected.\n");
+    //     if(pthread_create(&tid[i], NULL, client_handler, &client_sockfd) != 0)
+    //         printf("[-]Thread creation error.\n");
+    // }
+
+    //for reading commands
+
+    //connecting to client
+    server_sock_2 = socket(AF_INET, SOCK_DGRAM, 0);
+    if(server_sock_2 < 0)
+    {
+        perror("[-]Socket error");
+        exit(1);
+    }
+    memset(&server_addr_2, '\0', sizeof(server_addr_2));
+    server_addr_2.sin_family = AF_INET;
+    server_addr_2.sin_port = htons(client_port);
+    server_addr_2.sin_addr.s_addr = inet_addr(ip);
+    if(bind(server_sock_2, (struct sockaddr*)&server_addr_2, sizeof(server_addr_2)) < 0)
+    {
+        perror("[-]Bind error");
+        exit(1);
+    }
+    
+    //connecting to naming server
+    server_sock_1 = socket(AF_INET, SOCK_DGRAM, 0);
+    if(server_sock_1 < 0)
+    {
+        perror("[-]Socket error");
+        exit(1);
+    }
+    memset(&server_addr_1, '\0', sizeof(server_addr_1));
+    server_addr_1.sin_family = AF_INET;
+    server_addr_1.sin_port = htons(nm_port);
+    server_addr_1.sin_addr.s_addr = inet_addr(ip);
+    if(bind(server_sock_1, (struct sockaddr*)&server_addr_1, sizeof(server_addr_1)) < 0)
+    {
+        perror("[-]Bind error");
+        exit(1);
+    }
+
+    //separate threads for clients and naming server
+    pthread_t input[2];
+    addr_size = sizeof(server_addr_1);
+    args args_nm = (args)malloc(sizeof(threadargs));
+    args_nm->addr_size = addr_size;
+    args_nm->server_addr = server_addr_1;
+    args_nm->server_sock = server_sock_1;
+    args_nm->sockfd = nm_sockfd;
+    args_nm->buffer = buffer_nm;
+    nm_handler_for_ops((void*)args_nm);
+    // if(pthread_create(&input[0], NULL, nm_handler_for_ops, (void*)args_nm) != 0)
+    //     printf("[-]Thread creation error.\n");
+
+    // addr_size = sizeof(server_addr_2);
+    // args args_client = (args)malloc(sizeof(threadargs));
+    // args_client->addr_size = addr_size;  
+    // args_client->server_addr = server_addr_2;
+    // args_client->server_sock = server_sock_2;
+    // args_client->sockfd = client_sockfd;
+    // args_client->buffer = buffer_client;
+    // if(pthread_create(&input[1], NULL, client_handler, args_client) != 0)
+    //     printf("[-]Thread creation error.\n");
+
+    // while(1)
+    // {
+    //     bzero(buffer_nm, 1024);
+    //     if(recv(nm_sockfd, buffer_nm, sizeof(buffer_nm), 0) < 0)
+    //     {
+    //         perror("[-]Receive error");
+    //         exit(1);
+    //     }
+    //     if(strcmp(buffer_nm, "create") == 0)
+    //     {
+    //         bzero(buffer_nm, 1024);
+    //         strcpy(buffer_nm, "Send name of file");
+    //         if(send(nm_sockfd, buffer_nm, sizeof(buffer_nm), 0) < 0)
+    //         {
+    //             perror("[-]Send error");
+    //             exit(1);
+    //         }
+    //         bzero(buffer_nm, 1024);
+    //         if(recv(nm_sockfd, buffer_nm, sizeof(buffer_nm), 0) < 0)
+    //         {
+    //             perror("[-]Receive error");
+    //             exit(1);
+    //         }
+
+    //         int ack = open(buffer_nm, O_WRONLY | O_CREAT, 0644);
+    //         bzero(buffer_nm, 1024);
+    //         sprintf(buffer_nm, "%d", ack);
+    //         if(send(nm_sockfd, buffer_nm, sizeof(buffer_nm), 0) < 0)
+    //         {
+    //             perror("[-]Send error");
+    //             exit(1);
+    //         }
+    //         close(ack);
+    //     }
+    //     else if(strcmp(buffer_nm, "delete") == 0)
+    //     {
+    //         bzero(buffer_nm, 1024);
+    //         strcpy(buffer_nm, "Send name of file");
+    //         if(send(nm_sockfd, buffer_nm, sizeof(buffer_nm), 0) < 0)
+    //         {
+    //             perror("[-]Send error");
+    //             exit(1);
+    //         }
+    //         bzero(buffer_nm, 1024);
+    //         if(recv(nm_sockfd, buffer_nm, sizeof(buffer_nm), 0) < 0)
+    //         {
+    //             perror("[-]Receive error");
+    //             exit(1);
+    //         }
+        
+    //         int ack = remove(buffer_nm);
+    //         bzero(buffer_nm, 1024);
+    //         sprintf(buffer_nm, "%d", &ack);
+    //         if(send(nm_sockfd, buffer_nm, sizeof(buffer_nm), 0) < 0)
+    //         {
+    //             perror("[-]Send error");
+    //             exit(1);
+    //         }
+    //     }
+    //     else if(strcmp(buffer_nm, "COPY") == 0)
+    //     {
+
+    //     }
+    //     else if(strcmp(buffer_nm, "EXIT") == 0)
+    //     {
+    //         printf("[+]Exiting.\n");
+    //         break;
+    //     }
+    //     else
+    //     {
+    //         printf("[-]Invalid command received.\n");
+    //         break;
+    //     }
+    // }
 }
